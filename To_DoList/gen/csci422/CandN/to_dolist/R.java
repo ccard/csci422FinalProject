1be4adbffe90d2b651471889fc6304162ce7f385
Chris Card <ccard@mymail.mines.edu>
2012-10-24 20:13:56 -0600

    updated layout activity_to_do and the auto generated R file

--- To_DoList/gen/csci422/CandN/to_dolist/R.java
+++ To_DoList/gen/csci422/CandN/to_dolist/R.java
@@ -16,10 +16,15 @@ public final class R {
         public static final int ic_menu_add=0x7f020002;
     }
     public static final class id {
-        public static final int add=0x7f070000;
+        public static final int add=0x7f070004;
+        public static final int check=0x7f070001;
+        public static final int date=0x7f070003;
+        public static final int list=0x7f070000;
+        public static final int title=0x7f070002;
     }
     public static final class layout {
         public static final int activity_to_do=0x7f030000;
+        public static final int row=0x7f030001;
     }
     public static final class menu {
         public static final int activity_to_do=0x7f060000;
@@ -27,6 +32,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int menuNotice=0x7f040004;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_to_do=0x7f040003;
     }
