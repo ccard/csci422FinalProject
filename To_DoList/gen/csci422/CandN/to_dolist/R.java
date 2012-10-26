f26c17d1f34f74d2ac516beb0b5ca3a936098642
Chris Card <ccard@mymail.mines.edu>
2012-10-26 15:18:43 -0600

    R resource updated automagically

--- To_DoList/gen/csci422/CandN/to_dolist/R.java
+++ To_DoList/gen/csci422/CandN/to_dolist/R.java
@@ -14,29 +14,38 @@ public final class R {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int ic_menu_add=0x7f020002;
+        public static final int widget_frame=0x7f020003;
     }
     public static final class id {
-        public static final int add=0x7f070004;
-        public static final int check=0x7f070001;
-        public static final int date=0x7f070003;
-        public static final int list=0x7f070000;
-        public static final int title=0x7f070002;
+        public static final int add=0x7f080006;
+        public static final int add1=0x7f080005;
+        public static final int check=0x7f080001;
+        public static final int date=0x7f080003;
+        public static final int list=0x7f080000;
+        public static final int restaurants=0x7f080004;
+        public static final int title=0x7f080002;
     }
     public static final class layout {
         public static final int activity_to_do=0x7f030000;
         public static final int row=0x7f030001;
+        public static final int widget=0x7f030002;
+        public static final int widget_row=0x7f030003;
     }
     public static final class menu {
-        public static final int activity_to_do=0x7f060000;
+        public static final int activity_to_do=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menuNotice=0x7f040004;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_to_do=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int empty=0x7f050005;
+        public static final int hello_world=0x7f050001;
+        public static final int menuNotice=0x7f050004;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_to_do=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
+    }
+    public static final class xml {
+        public static final int widget_provider=0x7f040000;
     }
 }
