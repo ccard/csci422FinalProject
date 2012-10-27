3e88dd7ae06dfcd15fe6e881d8f282f749fb0812
Chris Card <ccard@mymail.mines.edu>
2012-10-27 16:34:38 -0600

    added DetailForm activity that will retreve user input from a detail form and save it

---
+++ To_DoList/src/csci422/CandN/to_dolist/DetailForm.java
@@ -0,0 +1,23 @@
+/*
+ *Chris Card
+ *Nathan Harvey
+ *10/27/12
+ *This class contains the code for retrieving and saving users input to a new task or modification of an old task 
+ */
+package csci422.CandN.to_dolist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailForm extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.detail_form);
+		
+		
+	}
+
+}
