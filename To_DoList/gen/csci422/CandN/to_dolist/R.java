8772203219ef2f4b1ff9c06ea2462aaf9ade917c
N Harvey <nthnhrvy@gmail.com>
2012-11-03 15:25:47 -0600

    Adding lots of stuff to DetailView (no data saving)

--- To_DoList/gen/csci422/CandN/to_dolist/R.java
+++ To_DoList/gen/csci422/CandN/to_dolist/R.java
@@ -8,9 +8,6 @@
 package csci422.CandN.to_dolist;
 
 public final class R {
-    public static final class array {
-        public static final int spinnerValues=0x7f060000;
-    }
     public static final class attr {
     }
     public static final class drawable {
@@ -25,21 +22,26 @@ public final class R {
         public static final int widget_frame=0x7f020008;
     }
     public static final class id {
-        public static final int Priority0=0x7f0a0004;
-        public static final int Priority1=0x7f0a0005;
-        public static final int Priority2=0x7f0a0006;
-        public static final int Priorityq=0x7f0a0003;
-        public static final int add=0x7f0a000e;
-        public static final int add1=0x7f0a000d;
-        public static final int check=0x7f0a0009;
-        public static final int completion=0x7f0a0008;
-        public static final int date=0x7f0a000b;
-        public static final int doneButton=0x7f0a0002;
-        public static final int newTypeTask=0x7f0a0000;
-        public static final int spinner1=0x7f0a0007;
-        public static final int taskName=0x7f0a0001;
-        public static final int tasks=0x7f0a000c;
-        public static final int title=0x7f0a000a;
+        public static final int Priorities=0x7f090003;
+        public static final int Priority0=0x7f090005;
+        public static final int Priority1=0x7f090006;
+        public static final int Priority2=0x7f090007;
+        public static final int Priorityq=0x7f090004;
+        public static final int add=0x7f090013;
+        public static final int add1=0x7f090012;
+        public static final int check=0x7f09000e;
+        public static final int completion=0x7f09000d;
+        public static final int date=0x7f090010;
+        public static final int doneButton=0x7f090002;
+        public static final int dueDatePicker=0x7f090009;
+        public static final int layoutCompletion=0x7f09000c;
+        public static final int layoutDueDate=0x7f090008;
+        public static final int layoutPickList=0x7f09000a;
+        public static final int newTypeTask=0x7f090000;
+        public static final int pickList=0x7f09000b;
+        public static final int taskName=0x7f090001;
+        public static final int tasks=0x7f090011;
+        public static final int title=0x7f09000f;
     }
     public static final class integer {
         public static final int maxline=0x7f050000;
@@ -52,21 +54,21 @@ public final class R {
         public static final int widget_row=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_to_do=0x7f090000;
+        public static final int activity_to_do=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f070000;
-        public static final int empty=0x7f070005;
-        public static final int hello_world=0x7f070001;
-        public static final int menuNotice=0x7f070004;
-        public static final int menu_settings=0x7f070002;
-        public static final int newtask_hint=0x7f070007;
-        public static final int ques=0x7f070008;
-        public static final int title_activity_detail_form=0x7f070006;
-        public static final int title_activity_to_do=0x7f070003;
+        public static final int app_name=0x7f060000;
+        public static final int empty=0x7f060005;
+        public static final int hello_world=0x7f060001;
+        public static final int menuNotice=0x7f060004;
+        public static final int menu_settings=0x7f060002;
+        public static final int newtask_hint=0x7f060007;
+        public static final int ques=0x7f060008;
+        public static final int title_activity_detail_form=0x7f060006;
+        public static final int title_activity_to_do=0x7f060003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f080000;
+        public static final int AppTheme=0x7f070000;
     }
     public static final class xml {
         public static final int widget_provider=0x7f040000;
--- To_DoList/src/csci422/CandN/to_dolist/DetailForm.java
+++ To_DoList/src/csci422/CandN/to_dolist/DetailForm.java
@@ -6,22 +6,70 @@
  */
 package csci422.CandN.to_dolist;
 
+import java.sql.Date;
+import java.text.AttributedString;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ExpandableListView;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
-//Comment out stuff here and in XML file
-//And see what is causing the bug.
 public class DetailForm extends Activity {
+	public static final String tag = "todoDetail";
+	private ImageButton[] priors = new ImageButton[4];
+	private DatePicker datepick;
+	private ExpandableListView pickList;
+	private String[] Listnames = {"Main","Homework","Shopping"};
 	private SeekBar completion;
+	/** -1 is ?, 0 is a dot, 1 is one !, 2 is two !!  */
+	private int priority;
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.detail_form);
 		completion = (SeekBar) findViewById(R.id.completion);
-		float per = completion.getProgress()/completion.getMax();
-		Toast.makeText(this, ""+per, Toast.LENGTH_LONG).show();
+		priors[0] = (ImageButton) findViewById(R.id.Priorityq);
+		priors[1] = (ImageButton) findViewById(R.id.Priority0);
+		priors[2] = (ImageButton) findViewById(R.id.Priority1);
+		priors[3] = (ImageButton) findViewById(R.id.Priority2);
+		datepick = ((DatePicker) findViewById(R.id.dueDatePicker));
+		pickList = ((ExpandableListView) findViewById(R.id.pickList));
+		for(String listname:Listnames){
+			TextView t = new TextView(this);
+			t.setText(listname);
+			pickList.addView(t, 50, 20);
+		}
 	}
+	public void saveStuff(View v){
+		Log.v(tag, "Progress: "+completion.getProgress());
+		Log.v(tag, completion.getKeyProgressIncrement()+" was done with keys");
+		Log.v(tag, "Secondary progress: "+completion.getSecondaryProgress());
+		Log.v(tag, "Thumb offset: "+completion.getThumbOffset());
+		Log.v(tag, "Max is: "+completion.getMax());
+		float pro = completion.getProgress()/((float)completion.getMax());
+		//have to cast to avoid integer division.
+		Toast.makeText(this, "I don't know how to save stuff!"+pro, Toast.LENGTH_LONG).show();
+	}
+	public void priq(View v){priority=-1;clr(v);}
+	public void prin(View v){priority=0;clr(v);}
+	public void prio(View v){priority=1;clr(v);}
+	public void prit(View v){priority=2;clr(v);}
 
+	/**
+	 * Clears the backgrounds for all priority buttons except one.
+	 * @param v the view to give an active background
+	 */
+	public void clr(View v){
+		Log.d(tag, "Priority: "+priority);
+		for(ImageButton b : priors){
+			b.setBackgroundResource(R.drawable.priorityblank);
+		}
+		v.setBackgroundResource(R.drawable.widget_frame);
+	}
 }
