8b2b216b01c4642955e79f3d9c47bc2813e813f2
Chris Card <ccard@mymail.mines.edu>
2012-11-06 19:30:31 -0700

    stores the lists the preference activities will sort by

--- To_DoList/gen/csci422/CandN/to_dolist/R.java
+++ To_DoList/gen/csci422/CandN/to_dolist/R.java
@@ -8,6 +8,10 @@
 package csci422.CandN.to_dolist;
 
 public final class R {
+    public static final class array {
+        public static final int sort_clauses=0x7f050001;
+        public static final int sort_titles=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -15,44 +19,45 @@ public final class R {
         public static final int ic_action_search=0x7f020001;
         public static final int ic_launcher=0x7f020002;
         public static final int ic_menu_add=0x7f020003;
-        public static final int priority1=0x7f020004;
-        public static final int priority2=0x7f020005;
-        public static final int priorityblank=0x7f020006;
-        public static final int prioritydot=0x7f020007;
-        public static final int priorityq=0x7f020008;
-        public static final int treasuremap=0x7f020009;
-        public static final int widget_frame=0x7f02000a;
+        public static final int ic_menu_settings_holo_light=0x7f020004;
+        public static final int priority1=0x7f020005;
+        public static final int priority2=0x7f020006;
+        public static final int priorityblank=0x7f020007;
+        public static final int prioritydot=0x7f020008;
+        public static final int priorityq=0x7f020009;
+        public static final int treasuremap=0x7f02000a;
+        public static final int widget_frame=0x7f02000b;
     }
     public static final class id {
-        public static final int Priorities=0x7f090003;
-        public static final int Priority0=0x7f090005;
-        public static final int Priority1=0x7f090006;
-        public static final int Priority2=0x7f090007;
-        public static final int Priorityq=0x7f090004;
-        public static final int add=0x7f090019;
-        public static final int add1=0x7f090017;
-        public static final int check=0x7f090013;
-        public static final int completion=0x7f090010;
-        public static final int date=0x7f090015;
-        public static final int deletebutton=0x7f090012;
-        public static final int doneButton=0x7f090002;
-        public static final int dueDatePicker=0x7f090009;
-        public static final int layoutCompletion=0x7f09000f;
-        public static final int layoutDueDate=0x7f090008;
-        public static final int layoutHere=0x7f09000b;
-        public static final int layoutPickList=0x7f09000d;
-        public static final int location=0x7f09000c;
-        public static final int name=0x7f090016;
-        public static final int newTypeTask=0x7f090000;
-        public static final int notes=0x7f090011;
-        public static final int pickDateButton=0x7f09000a;
-        public static final int pickList=0x7f09000e;
-        public static final int taskName=0x7f090001;
-        public static final int tasks=0x7f090018;
-        public static final int title=0x7f090014;
+        public static final int Priorities=0x7f0a0003;
+        public static final int Priority0=0x7f0a0005;
+        public static final int Priority1=0x7f0a0006;
+        public static final int Priority2=0x7f0a0007;
+        public static final int Priorityq=0x7f0a0004;
+        public static final int add=0x7f0a0019;
+        public static final int add1=0x7f0a0017;
+        public static final int check=0x7f0a0013;
+        public static final int completion=0x7f0a0010;
+        public static final int date=0x7f0a0015;
+        public static final int deletebutton=0x7f0a0012;
+        public static final int doneButton=0x7f0a0002;
+        public static final int dueDatePicker=0x7f0a0009;
+        public static final int layoutCompletion=0x7f0a000f;
+        public static final int layoutDueDate=0x7f0a0008;
+        public static final int layoutHere=0x7f0a000b;
+        public static final int layoutPickList=0x7f0a000d;
+        public static final int location=0x7f0a000c;
+        public static final int name=0x7f0a0016;
+        public static final int newTypeTask=0x7f0a0000;
+        public static final int notes=0x7f0a0011;
+        public static final int pickDateButton=0x7f0a000a;
+        public static final int pickList=0x7f0a000e;
+        public static final int taskName=0x7f0a0001;
+        public static final int tasks=0x7f0a0018;
+        public static final int title=0x7f0a0014;
     }
     public static final class integer {
-        public static final int maxline=0x7f050000;
+        public static final int maxline=0x7f060000;
     }
     public static final class layout {
         public static final int activity_to_do=0x7f030000;
@@ -62,23 +67,24 @@ public final class R {
         public static final int widget_row=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_to_do=0x7f080000;
+        public static final int activity_to_do=0x7f090000;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int empty=0x7f060005;
-        public static final int hello_world=0x7f060001;
-        public static final int menuNotice=0x7f060004;
-        public static final int menu_settings=0x7f060002;
-        public static final int newtask_hint=0x7f060007;
-        public static final int ques=0x7f060008;
-        public static final int title_activity_detail_form=0x7f060006;
-        public static final int title_activity_to_do=0x7f060003;
+        public static final int app_name=0x7f070000;
+        public static final int empty=0x7f070005;
+        public static final int hello_world=0x7f070001;
+        public static final int menuNotice=0x7f070004;
+        public static final int menu_settings=0x7f070002;
+        public static final int newtask_hint=0x7f070007;
+        public static final int ques=0x7f070008;
+        public static final int title_activity_detail_form=0x7f070006;
+        public static final int title_activity_to_do=0x7f070003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f070000;
+        public static final int AppTheme=0x7f080000;
     }
     public static final class xml {
-        public static final int widget_provider=0x7f040000;
+        public static final int preferences=0x7f040000;
+        public static final int widget_provider=0x7f040001;
     }
 }
