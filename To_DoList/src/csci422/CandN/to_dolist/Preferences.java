24087df022a0d9d0729a64f6891bf6434b729a8e
Chris Card <ccard@mymail.mines.edu>
2012-11-15 17:04:53 -0700

    updated preference activity with a preferencechange listener so that when the user desides to change the sync preferences it updates the flags in FileSync class

--- To_DoList/src/csci422/CandN/to_dolist/Preferences.java
+++ To_DoList/src/csci422/CandN/to_dolist/Preferences.java
@@ -7,6 +7,7 @@
 package csci422.CandN.to_dolist;
 
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
@@ -29,10 +30,48 @@ public class Preferences extends PreferenceActivity {
 		super.onResume();
 
 		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		prefs.registerOnSharedPreferenceChangeListener(onChange);
 	}
 	@Override 
 	public void onPause()
 	{
 		super.onPause();
+		prefs.unregisterOnSharedPreferenceChangeListener(onChange);
 	}
+
+
+
+	OnSharedPreferenceChangeListener onChange = new OnSharedPreferenceChangeListener()
+	{
+		public void onSharedPreferenceChanged(SharedPreferences prefs, String key)
+		{
+			if ("syncCal".equals(key)) 
+			{
+				boolean enabled = prefs.getBoolean(key, false);
+				boolean cal = prefs.getBoolean("syncWcal", false);
+				if (enabled && cal && !FileSync.getInstance().isSyncCal()) 
+				{
+					FileSync.getInstance().toggleCalSync();
+				}
+				else if (!enabled && FileSync.getInstance().isSyncCal()) 
+				{
+					FileSync.getInstance().toggleCalSync();
+				}
+				
+			}
+			else if("syncWcal".equals(key))
+			{
+				boolean cal = prefs.getBoolean(key, false);
+				if (cal && !FileSync.getInstance().isSyncCal()) 
+				{
+					FileSync.getInstance().toggleCalSync();
+				}
+				else if (!cal && FileSync.getInstance().isSyncCal()) 
+				{
+					FileSync.getInstance().toggleCalSync();
+				}
+				
+			}
+		}
+	};
 }
