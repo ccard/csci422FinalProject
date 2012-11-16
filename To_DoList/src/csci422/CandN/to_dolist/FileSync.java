0ad9e10e26e555f8e4038918d144c58602f78b4d
Chris Card <ccard@mymail.mines.edu>
2012-11-15 17:09:08 -0700

    added all nessacary method functionality and method stubs nathan to do the syncWithCal and saveToFile method bodies

--- To_DoList/src/csci422/CandN/to_dolist/FileSync.java
+++ To_DoList/src/csci422/CandN/to_dolist/FileSync.java
@@ -8,15 +8,38 @@
 
 package csci422.CandN.to_dolist;
 
+import android.database.Cursor;
+
 public class FileSync {
 
+	//Singleton patter stores its own instance
 	private static FileSync instance = null;
+
+	//this flag will be set true if the user updated preferences to
+	//sync with google cal
+	private boolean toSyncCal;
+
+	//to be used later but for sync the data base
+	//with the todo.txt file format or just a file
+	private boolean toSaveFile;
 	
+	/**
+	 * Private constructor so that this class controls
+	 * and ensures only one instance of the class
+	 */
 	private FileSync()
 	{
-		
+		toSyncCal = false;
+		toSaveFile = false;
+
 	}
 	
+	/**
+	 * This class returns an instance of its self if instance exists
+	 * or creates a new instance (enforces the singleton pattern)
+	 * @return: new instance if one doesn't exist or a single instance
+	 * 			of the class
+	 */
 	public static FileSync getInstance()
 	{
 		if(instance == null)
@@ -26,4 +49,87 @@ public class FileSync {
 		
 		return instance;
 	}
+
+	/**
+	 * Toggles toSyncCal to the opposite value of what it is at the moment
+	 */
+	public void toggleCalSync()
+	{
+		//toggles toSync to the opposite of what it is 
+		toSyncCal = (toSyncCal ? false : true);
+	}
+
+	/**
+	 * This method returns if it is to sync with google calender
+	 * @return: true if it is to sync with file
+	 */
+	public boolean isSyncCal()
+	{
+		return toSyncCal;
+	}
+
+	/**
+	 * This method returns if it is to sync with google calender
+	 * @return: true if it is to sync with file
+	 */
+	public boolean isSaveFile()
+	{
+		return toSaveFile;
+	}
+
+	/**
+	 * Toggles toSaveFile to the opposite value of what it is at the moment
+	 * This alows us to set the value from another file with out having to know its value
+	 */
+	public void toggleSaveFile()
+	{
+		toSaveFile = (toSaveFile ? false : true);
+	}
+
+
+	/**
+	 * WARNING: ONLY CALL THIS FUNCTION FROM AN INTENTSERVICE OR RISK SLOWING DOWN
+	 *			THE UI THREAD OR HAVE IT STOP BECAUSE USER CLOSES APP
+	 *
+	 * This function saves the entire data base stored in ToDoHelper to the
+	 * CalenderContract api (aka google calendar)
+	 * 
+	 * @param: ToDoHelper that has the data base since this class can't get its
+	 *			calling context.
+	 */
+	public void syncWithCal(ToDoHelper help)
+	{
+		if (toSyncCal) 
+		{
+			Cursor c = help.getAll("title");
+			c.moveToFirst();
+
+			//TODO: Nathan put the calander contract code here
+			
+		}//else do nothing
+	}
+
+	/**
+	 * WARNING: ONLY CALL THIS FUNCTION FROM AN INTENTSERVICE OR RISK SLOWING DOWN
+	 *			THE UI THREAD OR HAVE IT STOP BECAUSE USER CLOSES APP
+	 *
+	 * This function saves the entire data base stored in ToDoHelper to a
+	 * file the might be able to be moved to another app (to be implemented latter mabe)
+	 * 
+	 * @param: ToDoHelper that has the data base since this class can't get its
+	 *			calling context.
+	 */
+	public void saveToFile(ToDoHelper help)
+	{
+		if (toSaveFile) 
+		{
+			Cursor c = help.getAll("title");
+			c.moveToFirst();
+
+			//TODO: put function to save to the file
+			
+		}//else do nothing
+	}
+
+
 }
