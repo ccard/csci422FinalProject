a4debe9c0125f08b7d4cc5ff358358c9d6ec14cf
Chris Card <ccard@mymail.mines.edu>
2012-11-19 12:04:43 -0700

    removed unsued imports from these classes

--- To_DoList/src/csci422/CandN/to_dolist/ToDoHelper.java
+++ To_DoList/src/csci422/CandN/to_dolist/ToDoHelper.java
@@ -7,10 +7,8 @@
 package csci422.CandN.to_dolist;
 
 import android.content.Context;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteQueryBuilder;
 import android.database.Cursor;
 import android.content.ContentValues;
 
