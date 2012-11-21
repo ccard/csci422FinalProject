9af81a003a099fc79c58b374d80ff1ff193bb76b
N Harvey <nthnhrvy@gmail.com>
2012-11-21 10:17:03 -0700

    Attempting to add click action to the date text field

--- To_DoList/src/csci422/CandN/to_dolist/DetailForm.java
+++ To_DoList/src/csci422/CandN/to_dolist/DetailForm.java
@@ -380,7 +380,7 @@ public class DetailForm extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 	public void openCal(View v){ 
-
+		Log.v(tag, "Opening DatePicker Dialog...");
 		Builder calDialog = new Builder(this);
 		LayoutInflater inf = getLayoutInflater();
 		calDialog.setView(inf.inflate(R.layout.date_dialog, null));
